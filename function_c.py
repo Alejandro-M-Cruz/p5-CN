16b4646d6f814a884a3c721eb72de973ede8a899
Alejandro-M-Cruz <alejandromatiascc@gmail.com>
2023-12-12 14:03:22 +0000

    removed sending of unused message to SQS in function_c

--- function_c.py
+++ function_c.py
@@ -8,7 +8,6 @@ def lambda_handler(event, context):
     queue_url = os.environ["QUEUE_URL"]
     message = sqs.receive_message(QueueUrl=queue_url)["Messages"][0]["Body"] + " Hello from function C."
     print(message)
-    sqs.send_message(QueueUrl=queue_url, MessageBody=message)
     return {
         "message": message
     }
